a70231b2ff8715b293ec7ab2968f9f247d1d6384
Miguel Angel Zamora Carmona <maz191099@gmail.com>
2020-06-17 21:44:36 -0500

    Commit despues de Configurar la funcion __str__

--- listelement/models.py
+++ listelement/models.py
@@ -4,10 +4,14 @@ from django.db import models
 class Category(models.Model):
     title = models.CharField(max_length=255)
     url_clean = models.CharField(max_length=255)
+    def __str__(self):
+        return self.title
 
 class Type(models.Model):
     title = models.CharField(max_length=255)
     url_clean = models.CharField(max_length=255)
+    def __str__(self):
+        return self.title
 
 class Element(models.Model):
     title = models.CharField(max_length=255)
@@ -15,3 +19,5 @@ class Element(models.Model):
     description = models.TextField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     type = models.ForeignKey(Type, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.title
