0c892300b5e9d359c8acb760d317ebd83d8eae95
Miguel Angel Zamora Carmona <maz191099@gmail.com>
2020-06-17 21:28:37 -0500

    Commit despues de ejecutar las migraciones

--- productlist/settings.py
+++ productlist/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'productlist.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'productlist',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
